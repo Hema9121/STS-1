3daf3e9fe3f2ba79f3104acdefe2c16be58a2034
Hema9121 <hemasrinivasulu.ds@gmail.com>
2024-04-12 19:18:47 +0530

    2last commit

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-import pandas as pd
-import tensorflow as tf
-import tensorflow_hub as hub
+#import the required libraries and Load the Universal Sentence Encoder's TF Hub module
 
-from numpy import dot
-from numpy.linalg import norm
+import pandas as pd       # To work with tables
+import tensorflow as tf   # To work with USE4
+import tensorflow_hub as hub # contains USE4
 
-from flask import Flask,render_template,url_for,request
+from numpy import dot     # to calculate the dot product of two vectors
+from numpy.linalg import norm  #for finding the norm of a vector
+
+from flask import Flask,render_template,request
 
 module_url = "https://www.kaggle.com/models/google/universal-sentence-encoder/TensorFlow2/universal-sentence-encoder/2"
 model = hub.load(module_url)
@@ -16,9 +18,6 @@ def embed(input):
   return model(input)
 
 Data = pd.read_csv(r"C:\Users\dshem\OneDrive\Desktop\STS\STS-1\sts_data.csv")
-#message = [Data['text1'][0], Data['text2'][0]]
-#message_embeddings = embed(message)
-#message_embeddings
 
 app = Flask(__name__)
 
@@ -35,19 +34,18 @@ def predict():
         for i in ["text1","text2"]:
             dict1[i]=eval(i)
         ip = pd.DataFrame(dict1, index=[0])
-        ans = []                                                        
+        #Finding Cosine similarity
+        ans = []     # This list will contain the cosin similarity value for each vector pair present.                                                     
         for i in range(len(ip)):
-            messages = [ip['text1'][i], ip['text2'][i]]              
-            message_embeddings = embed(messages)                          
-            a = tf.make_ndarray(tf.make_tensor_proto(message_embeddings)) 
-            cos_sin = dot(a[0], a[1])/(norm(a[0])*norm(a[1]))             
-            ans.append(cos_sin)
+            messages = [ip['text1'][i], ip['text2'][i]]  #storing each sentence pair in messages            
+            message_embeddings = embed(messages)         #converting the sentence pair to vector pair using the embed() function                    
+            a = tf.make_ndarray(tf.make_tensor_proto(message_embeddings))   #storing the vector in the form of numpy array
+            cos_sin = dot(a[0], a[1])/(norm(a[0])*norm(a[1]))   #Finding the cosine between the two vectors          
+            ans.append(cos_sin)            #Appending the values into the ans list
         Ans = pd.DataFrame(ans, columns = ['similarity score'])
         ip = ip.join(Ans)
-        #ip['similarity score'] = ip['similarity score'] + 1
-        #ip['similarity score'] = ip['similarity score']/ip['similarity score'].abs().max()
         ip['similarity score'] = ip['similarity score'].apply(lambda x: round(x, 1))
-        result=ip[['similarity score']].to_dict("list")
+        result=ip[['similarity score']].to_dict("records")[0]
     return render_template('result.html',result=result)
 
 if __name__ == '__main__':
